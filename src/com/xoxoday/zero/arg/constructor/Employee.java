5831f9b2c2ab73933b7f28c281ef577bcc737e63
pravinklil <pravinklil1920@gmail.com>
2024-03-31 15:32:36 +0530

    initial commit

---
+++ src/com/xoxoday/zero/arg/constructor/Employee.java
@@ -0,0 +1,18 @@
+package com.xoxoday.zero.arg.constructor;
+
+public class Employee{
+	int i ; String s; int x; String v;
+
+	Employee(int i, String s){
+		int x = i;
+		String v = s;
+		
+		
+	}
+		
+	}
+
+	
+	
+
+
